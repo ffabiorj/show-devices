317044a35d0876b06a05e11fc4884ddff1d66108
Fábio Oliveira <fabio20rj@gmail.com>
2025-02-20 12:59:05 +0100

    update: Add user like a foreign key in device model

--- core/models.py
+++ core/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Device(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     ip = models.GenericIPAddressField()
     is_active = models.BooleanField()
